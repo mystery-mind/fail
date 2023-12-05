2ac54d18154835b3a762b92061edf07dd0bd76b1
mystery-mind <123958834+mystery-mind@users.noreply.github.com>
2023-12-05 12:41:58 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,17 @@
+peter_pan_song="";
+harry_potter_theme_song="";
+function setup(){
+    canvas=createCanvas(380,380);
+    canvas.center();
+
+    video = createCapture(VIDEO);
+    video.size(380,380);
+    video.hide();
+}
+function draw(){
+    image(video, 0 , 0 ,380, 380);
+}
+function preload(){
+    peter_pan_song=loadSound(music2.mp3);
+    harry_potter_theme_song=loadSound(music1.mp3);
+}
